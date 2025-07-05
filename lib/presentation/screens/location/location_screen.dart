be7b5d4ecae2dc84a05f1758d3830de72ea50e4a
Alan_Del10 <alaneduarddc10@outlook.com>
2025-07-05 14:22:15 -0600

    Finished location and map functionalities

--- lib/presentation/screens/location/location_screen.dart
+++ lib/presentation/screens/location/location_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
-class LocationScreen extends StatelessWidget {
+class LocationScreen extends ConsumerWidget {
   const LocationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Location Screen')));
+  Widget build(BuildContext context, ref) {
+    final userLocationProviderAsync = ref.watch(userLocationProvider);
+    final watchLocationProviderAsync = ref.watch(watchLocationProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Location Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Ubicación actual'),
+            userLocationProviderAsync.when(
+              data:
+                  (data) => Text(
+                    '(${data.$1.toStringAsFixed(4)}, ${data.$2.toStringAsFixed(4)})',
+                  ),
+              error:
+                  (error, stackTrace) => Text(
+                    'Error: $error',
+                    style: TextStyle(color: Colors.red),
+                  ),
+              loading: () => const CircularProgressIndicator(),
+            ),
+            const SizedBox(height: 30),
+            const Text('Seguimiento de ubicación'),
+            watchLocationProviderAsync.when(
+              data: (data) => Text('$data'),
+              error:
+                  (error, stackTrace) => Text(
+                    'Error: $error',
+                    style: TextStyle(color: Colors.red),
+                  ),
+              loading: () => const CircularProgressIndicator(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/screens/location/map_screen.dart
+++ lib/presentation/screens/location/map_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends ConsumerWidget {
   const MapScreen({super.key});
 
   @override
+  Widget build(BuildContext context, ref) {
+    final currentPosition = ref.watch(userLocationProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Map Screen')),
+      body: currentPosition.when(
+        data:
+            (data) =>
+                _MapView(initialLatitude: data.$1, initialLongitude: data.$2),
+        error:
+            (error, stackTrace) =>
+                Text('Error: $error', style: TextStyle(color: Colors.red)),
+        loading: () => const Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+}
+
+class _MapView extends StatefulWidget {
+  final double initialLatitude;
+  final double initialLongitude;
+
+  const _MapView({
+    required this.initialLatitude,
+    required this.initialLongitude,
+  });
+
+  @override
+  State<_MapView> createState() => __MapViewState();
+}
+
+class __MapViewState extends State<_MapView> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Map Screen')));
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: CameraPosition(
+        zoom: 12,
+        target: LatLng(widget.initialLatitude, widget.initialLongitude),
+      ),
+      myLocationEnabled: true,
+      onMapCreated: (GoogleMapController controller) {
+        //_controller.complete(controller);
+      },
+    );
   }
 }
